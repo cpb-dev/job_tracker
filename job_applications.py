ed1655ecd8054e8121d4f6f510b1a4d52e4130dd
Conor Bailey <cpbailey025@gmail.com>
2022-10-25 18:08:30 +0100

    Finished for now
    This is the final product for what I needed it for, however refinements may still be beneficial.
    The role of the application is fulfilled but I may want to build on this to make it better.

--- job_applications.py
+++ job_applications.py
@@ -1,4 +1,3 @@
-#TEMPORARY PROJECT
 #Used to collect information about jobs applied to and how many applications have been sent
 
 import csv
@@ -16,7 +15,7 @@ def read_applications():
 
 def new_application(company, role, date):
     with open('job_application.csv', mode='a') as job_application:
-        application_writer = csv.DictWriter(job_application, fieldnames= field_names)
+        application_writer = csv.DictWriter(job_application, fieldnames = field_names)
 
         application_writer.writerow({'company': company, 'role': role, 'date_applied': date})
 
@@ -35,5 +34,27 @@ def job_application():
 
     new_application(inp_comp, inp_role, date)
 
-job_application()
-    
+#Program
+
+print("Welcome to the job application bot!")
+print("Here I save the jobs you've applied to.")
+
+u_inp = input(
+    "What would you like to do today? \n Type the number of the option you want! \n 1. New Job Application \n 2. View all Applications \n 3. Search Applications \n Choice: "
+)
+
+if(u_inp == "1"):
+    job_application()
+elif(u_inp == "2"):
+    read_applications()
+elif(u_inp == "3"):
+    search = input(
+        "What would you like to search by? \n 1. Job Role \n 2. Company \n Choice: "
+    )
+    if(search == "1"):
+        role = input("What role are you looking for?: ")
+        search_roles(role)
+
+    elif(search == "2"):
+        company = input("What company are you looking for?: ")
+        search_company(company)
